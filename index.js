dcf352faadabd0f70ac03c664f8fe6446781890e
Anwar4123 <anwarhossenmehidi@gmail.com>
2020-10-14 13:53:00 -0700

    adede

---
+++ index.js
@@ -0,0 +1,152 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+const fileUpload =require("express-fileupload");
+const cors = require('cors')
+require('dotenv').config()
+
+
+const uri = "mongodb+srv://anwarkhan:anwarkhan2391@cluster0.ckhne.mongodb.net/agency?retryWrites=true&w=majority";
+
+ //const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS} <@cluster0.ckhne.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+
+const app = express()
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.use(fileUpload())
+
+const port = 5000
+
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true});
+client.connect(err => {
+  const appointmentCollection = client.db("agency").collection("order");
+  const feedbackCollection = client.db("agency").collection("feedback");
+  const adminCollection = client.db("agency").collection("admin");
+  const serviceCollection = client.db("agency").collection("service");
+
+    app.post('/addOrder', (req, res) => {
+        const appointment = req.body;
+        console.log(appointment);
+        appointmentCollection.insertOne(appointment)
+            .then(result => {
+                res.send(result.insertedCount > 0)
+            })
+    });
+
+    app.get('/orders', (req, res) => {
+        appointmentCollection.find({})
+            .toArray((err, documents) => {
+                res.send(documents);
+            })
+    })
+
+    app.post('/AddFeedback', (req, res) => {
+        const feedbacks = req.body;
+        console.log(feedbacks);
+        feedbackCollection.insertOne(feedbacks)
+            .then(result => {
+                res.send(result.insertedCount > 0)
+            })
+    });
+
+    app.get('/feedbackArea', (req, res) => {
+        feedbackCollection.find({})
+        .toArray((err, documents) => {
+            res.send(documents)
+        })
+    })
+
+
+    
+
+    // app.post('/appointmentsByDate', (req, res) => {
+    //     const date = req.body;
+    //     const email = req.body.email;
+    //     serviceCollection.find({ email: email })
+    //         .toArray((err, doctors) => {
+    //             const filter = { date: date.date }
+    //             if (doctors.length === 0) {
+    //                 filter.email = email;
+    //             }
+    //             appointmentCollection.find(filter)
+    //                 .toArray((err, documents) => {
+    //                     console.log(email, date.date, doctors, documents)
+    //                     res.send(documents);
+    //                 })
+    //         })
+    // })
+
+    app.post('/addAService', (req, res) => {
+        const file = req.files.file;
+        const name = req.body.name;
+        const description = req.body.description;
+        const email = req.body.email;
+        const newImg = file.data;
+        const encImg = newImg.toString('base64');
+
+        var image = {
+            contentType: file.mimetype,
+            size: file.size,
+            img: Buffer.from(encImg, 'base64')
+        };
+
+        serviceCollection.insertOne({ name, email, image, description })
+            .then(result => {
+                res.send(result.insertedCount > 0);
+            })
+    })
+
+    app.post('/addAdmin', (req, res) => {
+        const email = req.body.email;
+        adminCollection.insertOne({ email })
+            .then(result => {
+                res.send(result.insertedCount > 0);
+            })
+    })
+
+    app.get('/service', (req, res) => {
+        serviceCollection.find({})
+            .toArray((err, documents) => {
+                res.send(documents);
+            })
+    });
+
+    app.post('/isAdmin', (req, res) => {
+        const email = req.body.email;
+        console.log(email);
+        adminCollection.find({ email: email })
+            .toArray((err, documents) => {
+                res.send(documents.length > 0);
+            })
+    })
+
+
+
+
+//   app.post('/addDoctor', (req , res)=> {
+//     const file = req.files.file;
+//     const name = req.body.name;
+//     const email = req.body.email;
+//     console.log(name, email, file);
+//     file.mv(`${__dirname}/doctors/${file.name}`, err => {
+//       if (err) {
+//         console.log(err);
+//         return res.status(500).send({ msg: 'failed to upload image'});
+//       }
+//       return res.send({name: file.name, path: `/${file.name}`})
+//     })
+  
+//   })
+  
+});
+
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(process.env.PORT || port)
